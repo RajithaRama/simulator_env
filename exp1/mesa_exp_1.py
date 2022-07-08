a24fb7715aeb49afbfc2f8b18a06030814fb365e
rajitharama <rajitharamanayake@gmail.com>
2022-07-08 22:38:56 +0100

    visibility functions added.

--- exp1/mesa_exp_1.py
+++ exp1/mesa_exp_1.py
@@ -1,7 +1,78 @@
 from enum import Enum
+import numpy as np
 
 from mesa import Agent, Model, space, time
 
+import numpy as np
+def _bresenhamline_nslope(slope):
+    """
+    Normalize slope for Bresenham's line algorithm.
+    """
+    scale = np.amax(np.abs(slope), axis=1).reshape(-1, 1)
+    zeroslope = (scale == 0).all(1)
+    scale[zeroslope] = np.ones(1)
+    normalizedslope = np.array(slope, dtype=np.double) / scale
+    normalizedslope[zeroslope] = np.zeros(slope[0].shape)
+    return normalizedslope
+
+def _bresenhamlines(start, end, max_iter):
+    """
+    Returns npts lines of length max_iter each. (npts x max_iter x dimension)
+
+    array([[[ 3,  1,  8,  0],
+            [ 2,  1,  7,  0],
+            [ 2,  1,  6,  0],
+            [ 2,  1,  5,  0],
+            [ 1,  0,  4,  0],
+            [ 1,  0,  3,  0],
+            [ 1,  0,  2,  0],
+            [ 0,  0,  1,  0],
+            [ 0,  0,  0,  0]],
+    <BLANKLINE>
+           [[ 0,  0,  2,  0],
+            [ 0,  0,  1,  0],
+            [ 0,  0,  0,  0],
+            [ 0,  0, -1,  0],
+            [ 0,  0, -2,  0],
+            [ 0,  0, -3,  0],
+            [ 0,  0, -4,  0],
+            [ 0,  0, -5,  0],
+            [ 0,  0, -6,  0]]])
+    """
+    if max_iter == -1:
+        max_iter = np.amax(np.amax(np.abs(end - start), axis=1))
+    npts, dim = start.shape
+    nslope = _bresenhamline_nslope(end - start)
+
+    # steps to iterate on
+    stepseq = np.arange(1, max_iter + 1)
+    stepmat = np.tile(stepseq, (dim, 1)).T
+
+    # some hacks for broadcasting properly
+    bline = start[:, np.newaxis, :] + nslope[:, np.newaxis, :] * stepmat
+
+    # Approximate to nearest int
+    return np.array(np.rint(bline), dtype=start.dtype)
+
+def bresenhamline(start, end, max_iter=5):
+    """
+    Returns a list of points from (start, end] by ray tracing a line b/w the
+    points.
+    Parameters:
+        start: An array of start points (number of points x dimension)
+        end:   An end points (1 x dimension)
+            or An array of end point corresponding to each start point
+                (number of points x dimension)
+        max_iter: Max points to traverse. if -1, maximum number of required
+                  points are traversed
+
+    Returns:
+        linevox (n x dimension) A cumulative array of all points traversed by
+        all the lines so far.
+    """
+
+    # Return the points as a single array
+    return _bresenhamlines(start, end, max_iter).reshape(-1, start.shape[-1])
 
 class Direction(Enum):
     left = 0
@@ -128,8 +199,41 @@ class Home(Model):
         wall, end_id = self.add_vertical_wall(9, 9, 11, end_id)
         self.walls.append(wall)
 
-        self.grid.place_agent(self.patient, (5, 5))
+        self.grid.place_agent(self.patient, (6, 9))
         self.grid.place_agent(self.robot, (5, 6))
+
+    def visible_stakeholders(self):
+        neighbors = self.grid.get_neighbors(self.robot.pos, moore=True, radius=3)
+        print("neigh: " + str(neighbors))
+
+        coordinate_neighbors = {}
+        walls = {}
+        for neighbor in neighbors:
+            if neighbor.type == 'wall':
+                walls[neighbor.pos] = neighbor
+            else:
+                coordinate_neighbors[neighbor.pos] = neighbor
+
+        print(coordinate_neighbors)
+
+        visible_neighbors = []
+        for coordinate, neighbor in coordinate_neighbors.items():
+            robot_pos = np.array([np.array(self.robot.pos)])
+            print(robot_pos.shape)
+            # robot_pos = robot_pos.reshape(1, 1)
+            # print(robot_pos.shape)
+            visible_line = bresenhamline(np.array([np.array(self.robot.pos)]), np.array([np.array(neighbor.pos)]), -1)
+            # print(visible_line)
+            visible = True
+            for point in visible_line:
+                if tuple(point) in walls.keys():
+                    visible = False
+
+            if visible:
+                visible_neighbors.append(neighbor)
+
+        print("visible: " + str(visible_neighbors))
+
 
     def step(self):
         self.schedule.step()
@@ -157,3 +261,4 @@ class Home(Model):
 
 model = Home()
 model.step()
+model.visible_stakeholders()
