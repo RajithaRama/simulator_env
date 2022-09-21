8ea9526974be4bc838e5b7602cad562bdfe9fc77
rajitharama <rajitharamanayake@gmail.com>
2022-09-21 17:44:16 +0100

    sync 21/09

--- ethical_governor/blackboard/ethicaltests/elder_care_utilitarian_test.py
+++ ethical_governor/blackboard/ethicaltests/elder_care_utilitarian_test.py
@@ -17,21 +17,38 @@ class ElderCareUtilitarianTest(ethical_test.EthicalTest):
         env = data.get_environment_data()
         stakeholder_data = data.get_stakeholders_data()
 
+        utils = {}
         for action in data.get_actions():
             logger.info('Testing action: ' + str(action.value))
 
             logger.info('Calculating autonomy utility for stakeholders')
-            autonomy_utility = self.get_autonomy_utility(env=env, stakeholder_data=stakeholder_data, action=action, logger=logger)
+            autonomy_utility = self.get_autonomy_utility(env=env, stakeholder_data=stakeholder_data, action=action,
+                                                         logger=logger)
             logger.info('Autonomy utilities for action ' + str(action.value) + ': ' + str(autonomy_utility))
 
-    def get_autonomy_utility(self, env, stakeholder_data, action, logger):
+            logger.info('Calculating well-being utility for stakeholders')
+            wellbeing_util = self.get_wellbeing_utility(env=env, stakeholder_data=stakeholder_data, action=action,
+                                                        logger=logger)
 
+
+            utils.setdefault(action, {})['autonomy'] = autonomy_utility
+
+    def get_autonomy_utility(self, env, stakeholder_data, action, logger):
+        """
+        Calculating autonomy  values for stakeholders.
+        When the robot does not follow a user command, autonomy value for that user is -1, if it follows 1 and the
+        default is 0.
+        - return: list with (stakeholder_id, autonomy utility) tuples
+        """
         # Simulating next pos and visible lines
         next_pos = action.value[0](*action.value[1:], act=False)
-        # visible_stakeholders = stakeholder_data['robot']['model'].model.visible_stakeholders(center_agent_pos=next_pos, visibility_radius=ROBOT.VISIBLE_DIST)
+        # visible_stakeholders = stakeholder_data['robot']['model'].model.visible_stakeholders(
+        # center_agent_pos=next_pos, visibility_radius=ROBOT.VISIBLE_DIST)
         # visible_stakeholders_ids = [stakeholder.id for stakeholder in visible_stakeholders]
         robot_model = stakeholder_data['robot']['model']
         instruction_list = robot_model.model.get_commands(self)
+
+        stakholder_autonomy_values = []
 
         for stakeholder in stakeholder_data.keys():
             # skip for robot
@@ -55,20 +72,26 @@ class ElderCareUtilitarianTest(ethical_test.EthicalTest):
                         else:
                             autonomy_utility = -1
 
+            stakholder_autonomy_values.append((stakeholder, autonomy_utility))
 
+        return stakholder_autonomy_values
 
+    def get_wellbeing_utility(self, env, stakeholder_data, action, logger):
+        """
+        Calculating wellbeing utility values for stakeholders.
 
-            # autonomy_utility = 0
-            # if robot_model.not_follow_request and (robot_model.follow == action.value[0]) and stakeholder == 'follower':
-            #     autonomy_utility = -1
-            # elif robot_model.not_follow_request and stakeholder == 'follower':
-            #     autonomy_utility = 1
-            #
-            # if action.value[0] == robot_model.move_away:
-            #     if stakeholder_data[stakeholder]['id'] == action.value[1]:
-            #         autonomy_utility = 1
-            #     else:
-            #         auto
+        wellbeing = 2/(1+e^((x-(m+n))/2)) - 1
 
+        x = time from last seen
+        m = average time stay in
+        n = std time stay in
+        k = logistic growth rate
+        """
+        # Simulating next pos and visible lines
+        next_pos = action.value[0](*action.value[1:], act=False)
 
-        return autonomy_utility
+        visible_stakeholders = stakeholder_data['robot']['model'].model.visible_stakeholders(
+                                        center_agent_pos=next_pos, visibility_radius=ROBOT.VISIBLE_DIST)
+        visible_stakeholders_ids = [stakeholder.id for stakeholder in visible_stakeholders]
+
+        # for
--- ethical_governor/blackboard/evaluator/utilitarian_eldercare_evaluator.py
+++ ethical_governor/blackboard/evaluator/utilitarian_eldercare_evaluator.py
@@ -12,13 +12,13 @@ class UtilitarianEvaluator(evaluator.Evaluator):
         for action in data.get_actions():
             desirability = 0
             i = 0
-            for stakeholder in data.get_stakeholders_data().keys():
-                i += 1
-                if data.get_table_data(action, stakeholder + "_Wellbeing") == 0:
-                    desirability += data.get_table_data(action, stakeholder + "_Autonomy")
-                else:
-                    desirability += 2 * data.get_table_data(action, "Wellbeing") + 0.75 * (data.get_table_data(action,
-                                                                                                               "Autonomy") + data.get_table_data(action, "obedience"))
+            # for stakeholder in data.get_stakeholders_data().keys():
+            #     i += 1
+            #     if data.get_table_data(action, stakeholder + "_Wellbeing") == 0:
+            #         desirability += data.get_table_data(action, stakeholder + "_Autonomy")
+            #     else:
+            #         desirability += 2 * data.get_table_data(action, "Wellbeing") + 0.75 * (data.get_table_data(action,
+            #                                                                                                    "Autonomy") + data.get_table_data(action, "obedience"))
             logger.info('Desirability of action ' + str(action.value) + ' : ' + str(desirability))
             self.score[action] = desirability
 
