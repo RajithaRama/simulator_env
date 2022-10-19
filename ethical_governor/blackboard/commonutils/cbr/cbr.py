e8f679e507ef8cbe34fb0480b1d5139c156d61c5
rajitharama <rajitharamanayake@gmail.com>
2022-10-20 00:29:10 +0100

    Sync 20/10

--- ethical_governor/blackboard/commonutils/cbr/cbr.py
+++ ethical_governor/blackboard/commonutils/cbr/cbr.py
@@ -4,13 +4,48 @@ from sklearn.neighbors import NearestNeighbors
 
 
 class CBR:
-    def __init__(self, k=3, algorithm='brute'):
+    def __init__(self, k=3):
         self.col_names = None
-        self.data = NearestNeighbors(n_neighbors=k, metric=self.distance, algorithm=algorithm)
+        self.data = pd.DataFrame()
+        self.dist_feature_map = {}
 
-    def add_data(self, X):
-        self.data.fit(X)
-        self.col_names = self.data.feature_names_in_
+    def add_data(self, data):
+        self.data = data
+        self.col_names = self.data.get_table_col_names()
+
+    def get_neighbours(self, query, k=3):
+        q_col_names = query.col_names
+        distances = {}
+        data_inv = self.data.T
+        for col in data_inv.columns:
+            vec_s = data_inv[col]
+            in_same_f_space = True
+            for qcol in q_col_names:
+                if vec_s[qcol] is None:
+                    in_same_f_space = False
+                    break
+            if in_same_f_space:
+                distances.setdefault(self.distance(vec_s, query), []).append(vec_s['case_id'])
+                # distances[self.distance(vec_s, query)] = vec_s['case_id']
+
+        neighbours = []
+        while len(neighbours) <= k:
+            if len(distances[min(distances.keys())]) + len(neighbours) <= k:
+                for case in distances[min(distances.keys())]:
+                    neighbours.append(data_inv[case])
+            else:
+                i = len(neighbours)
+                for case in distances[min(distances.keys())]:
+                    if i > k:
+                        break
+                    neighbours.append(data_inv[case])
+                    i += 1
+        return neighbours
+
+
+
+
+
 
     def distance(self, a, b):
         return 1
--- ethical_governor/blackboard/evaluator/PSRB_eldercare_evaluator.py
+++ ethical_governor/blackboard/evaluator/PSRB_eldercare_evaluator.py
@@ -5,11 +5,13 @@ import ethical_governor.blackboard.evaluator.evaluator as evaluator
 from ethical_governor.blackboard.commonutils.cbr.cbr import CBR
 
 CASE_BASE = os.path.join(os.getcwd(), 'ethical_governor', 'blackboard', 'commonutils', 'cbr', 'casebase')
+
+
 class PSRBEvaluator(evaluator.Evaluator):
     def __init__(self):
         super().__init__()
         self.expert_db = CBR()
-        data_df = pd.DataFrame.load_csv(CASE_BASE)
+        data_df = pd.read_csv(CASE_BASE)
         self.expert_db.add_data(data_df)
 
     def evaluate(self, data, logger):
